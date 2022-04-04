dd99bf7acf3f5e1218e75b0c6f5798e562571c8b
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-04 10:45:48 -0400

    commit tree 0bea03147d6ef436b457bda03d29f825e09ad8e6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+let employeeRecord = {}
+function createEmployeeRecord([firstName, familyName, title], payPerHour) {
+    let timeInEvents = []
+    let timeOutEvents = [] 
+    let employeeRecord = { 
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: timeInEvents,
+        timeOutEvents: timeOutEvents
+    }
+}
+
+createEmployeeRecord(["Gray", "Worm", "Security"], 1)
+console.log(employeeRecord)
+//function createEmployeeRecords() {}
